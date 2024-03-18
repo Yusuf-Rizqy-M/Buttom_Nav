7ea2c5f69993f6ca4ea8f3afd13a475d107e3020
Yusuf-Rizqy-M <yusufhabib290@gmail.com>
2024-03-18 09:32:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/buttom_nav/HomeFragment.java
@@ -0,0 +1,19 @@
+package com.example.buttom_nav;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_h_o_m_e, container, false);
+
+    }
+}
